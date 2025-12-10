d8c9bab3b87604e1ed79fc65f1c227cb9f1acf74
notfroogs <Jehuang@s.sfusd.edu>
2025-12-10 13:30:32 -0800

    V2 done

--- test-game/Scene/battle.gd
+++ test-game/Scene/battle.gd
@@ -1,15 +1,35 @@
 extends Control
 
+enum States{
+	 OPTIONS,
+	TARGETS,
+}
+ 
 
 @onready var _options: WindowDefault = $options
 @onready var _options_menu: Menu = $options/optionss
+@onready var _enemies_menu: Menu = $enemies
+
+var state: States = States.OPTIONS
 
 func _ready() -> void:
 	_options_menu.button_focus(0)
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		match state:
+			States.OPTIONS:
+				pass
+			States.TARGETS:
+				state = States.OPTIONS
+				_options_menu.button_focus()
 
 
 func _on_optionss_button_focused(button: BaseButton) -> void:
 	pass
-func _on_optionss_button_pressed(button: BaseButton) -> void:
+func _on_optionss_button_pressed(button: BaseButton)  -> void:
 	print(button.text)
+	match button.text:
+		"ATTACK":
+			state = States.TARGETS
+			_enemies_menu.button_focus()
--- test-game/menu.gd
+++ test-game/menu.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 		button.focus_exited.connect(_on_Button_focus_exited.bind(button))
 		button.focus_entered.connect(_on_Button_focused.bind(button))
 		button.pressed.connect(_on_Button_pressed.bind(button))
-		
+	button_enable_focus(false)
 func get_buttons() -> Array:
 	return get_children()
 
@@ -22,19 +22,29 @@ func connect_to_buttons(target: Object, _name: String= name) -> void:
 	callable = Callable(target, "_on_" + _name + "_pressed")
 	button_pressed.connect(callable)
 
+func button_enable_focus(on:bool) ->void:
+	var mode: FocusMode = FocusMode.FOCUS_ALL if on else FocusMode.FOCUS_NONE
+	for button in get_buttons():
+		button.set_focus_mode(mode)
+
 func button_focus(n:int=index)->void:
+	button_enable_focus(true) 
 	var button : BaseButton = get_buttons()[n]
 	button.grab_focus()
 
 func _on_Button_focus_exited(button:BaseButton)->void:
-	#await get_tree().process_frame
+	await get_tree().process_frame 
 	if not get_viewport().gui_get_focus_owner() in get_buttons():
-		print("bring focus back")
-		button.grab_focus()
+		button_enable_focus(false)
+	##await get_tree().process_frame
+	#if not get_viewport().gui_get_focus_owner() in get_buttons():
+		#print("bring focus back")
+		#button.grab_focus()
 # Uhh i lowkey dont know why the focus is glitching out ill figure it out later its something with this function tho.
 # im pretty sure its something with the control nodes that were connected with this script
 func _on_Button_focused(button:BaseButton)->void:
-		emit_signal("button_focused",button)		
-		
+	index = button.get_index()
+	emit_signal("button_focused",button)
+	
 func _on_Button_pressed(button: BaseButton)->void:
 	emit_signal("button_pressed",button)
